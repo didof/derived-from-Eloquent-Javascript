8a019458392b191792f558e216c8fc63e7414f49
prostagma-code <didonato.fr@gmail.com>
2020-02-14 06:24:16 +0100

    "Update: functioning UI"

--- level-2/script.js
+++ level-2/script.js
@@ -51,7 +51,6 @@ const DisplayModule = (() => {
             reassignButtons(true);           
         },
         removeEvent: () => {
-
         let ID, row, buttons;
 
         ID = lastRem.id.split('-')[2];
@@ -74,6 +73,30 @@ const DisplayModule = (() => {
         document.querySelector('.patient_events').style.transition = "0.3s 0.1s ease";
 
         reassignButtons(false);
+        },
+        addPatientToList: (obj) => {
+            let ill, HTML, newHTML;
+            ill = "patient_row"
+
+            HTML = '<li class="%ill%" id="patient-%num%"><div class="patient_number">#%num%</div><div class="patient_bio">%lastname%, %firstname%</div><div class="patient_events">%events%</div><div class="patient_delete"><input type="button" class="patient_delete_btn" id="patient_delete_%num%" value="X"></div></li>';
+
+        obj.disease == false ? null : ill += " disease";
+        newHTML = HTML.replace('%ill%', ill);
+        newHTML = newHTML.replace(/%num%/g, obj.num);
+        newHTML = newHTML.replace('%lastname%', obj.lastName);
+        newHTML = newHTML.replace('%firstname%', obj.firstName);
+        newHTML = newHTML.replace('%events%', obj.eventArr.join(', '));
+
+        document.querySelector('.list_show').insertAdjacentHTML('beforeend', newHTML);
+        },
+        resertForm: (obj) => {
+            document.querySelector('.patient_label').textContent = "Patient #" + (obj.num + 1);
+        },
+        updateList: (crowd) => {
+            console.log(crowd);
+            for(let i = 0; i < crowd.length; i++) {
+                DisplayModule.addPatientToList(crowd[i]);
+            }
         }
     }
 })();
@@ -81,10 +104,63 @@ const DisplayModule = (() => {
 
 
 const LogicModule = (() => {
-    let firstName, lastName, eventArr, disease, patientObj, journal;
+    let firstName, lastName, eventArr, disease, patientObj, journal, num, randomArr, howManyEvents;
     
     eventArr = [];
     journal = [];
+    num = 1;
+
+    randomArr = ['dog', 'cat', 'apple', 'sun', 'breeze', 'clay', 'sea', 'shell', 'bread', 'dirt', 'grass', 'wine', 'beer']
+
+    const callRandomNumber = (min, max) => {
+        if(min == 0 && max == 1) { return; }
+        if(max - min == 1) { return; }
+        return Math.floor(Math.random() * (max - min - 1) + 1);
+    }
+
+    const randomBio = () => {
+        let firstArr, lastArr;
+        firstArr = ['Antonio', 'Leo', 'Luca', 'Marco', 'Giuseppe', 'Francesco', 'Carlo', 'Mattia', 'Matteo', 'Davide', 'Damiano', 'Gabriele', 'Fabrizio'];
+        lastArr = ['Del Latte', 'Di Muro', 'Zingarelli', 'Papagna', 'Di Nunno', 'Rosa', 'Quinto', 'Cancelli', 'Sorrenti', 'Cavalli', 'Tagliapietre', 'Del Vescovo', 'Trancoso'];
+
+        return {
+            first: firstArr[callRandomNumber(1, firstArr.length)],
+            last: lastArr[callRandomNumber(1, lastArr.length)]
+        }
+    }
+
+    const randomPatient = virality => {
+        
+        // from whic id I start counting? from num
+        let bio, randomObj, risk, ill, randomEvents;
+        randomEvents = [];
+
+        bio = randomBio();
+        risk = callRandomNumber(1, 100);
+        if (risk > virality) {
+            ill = false;
+        } else {
+            ill = true;
+        }
+
+    howManyEvents = callRandomNumber(1, 7);
+    for(let i = 0; i < howManyEvents; i++) {
+        let whichOne = callRandomNumber(1, randomArr.length);
+        randomEvents.push(randomArr[whichOne]);
+    }
+
+        randomObj = {
+                num: num,
+                firstName: bio.first,
+                lastName: bio.last,
+                eventArr: randomEvents,
+                disease: ill
+        }
+
+        num++;
+
+        return randomObj;
+    }
 
     return {
         test: () => {
@@ -97,18 +173,36 @@ const LogicModule = (() => {
             // get fields input, push to data-storage, than return it as an object
             firstName = document.querySelector('.input_firstname').value;
             lastName = document.querySelector('.input_lastname').value;
-            disease = document.querySelector('.input_disease').value;
+            disease = document.querySelector('.input_disease').checked;
 
             document.querySelectorAll('.input_event').forEach((record) => {
             eventArr.push(record.value);
             });
             
             // push to data-storage
-            patientObj = {firstName, lastName, eventArr, disease};
+            patientObj = {num, firstName, lastName, eventArr, disease};
             journal.push(patientObj);
 
+            // increment id
+            num++;
+
             // return it as object
-            return journal;
+            return patientObj;
+        },
+        generateRandom: (quantity, virality) => {
+            let crowdArr;
+            
+            crowdArr = [];
+
+            for(let i = 0; i < quantity; i++) {
+                crowdArr.push(randomPatient(virality));
+            }
+            return crowdArr;
+        },
+        addCrowd: (crowd) => {
+            for(let i = 0; i < crowd.length; i++) {
+                journal.push(crowd[i]);
+            }
         }
     }
 })();
@@ -140,20 +234,39 @@ const Controll = ((display, logic) => {
             }
         });
 
+            // random Handler
+            document.getElementById('randomHandler').addEventListener('click', insertRandomCrowd);
+
         //ogni volta che crei un nuovo event spostare il focus nel nuovo event
 
         
     }
 
+    const insertRandomCrowd = () => {
+        let quantity, virality, crowd, journalUpdated;
+
+        // ask how many
+        quantity = prompt("How many random patients do you want to inject?", 42);
+        virality = prompt("How high is the frequency of illness", 50);
+
+        // get back an array made up of objects
+        crowd = logic.generateRandom(quantity, virality);
+
+        // per each index of crowd array, update data in logic and show on UI
+        logic.addCrowd(crowd);
+        display.updateList(crowd);
+        display.resertForm(crowd[crowd.length - 1]);
+    }
+
     const getInput = function() {
         // ask LogicModule => get, push, return fieldsInput as an object, patientInput
-        let patientsJournal = logic.getInput();
-        console.log(patientsJournal);
+        let patientObj = logic.getInput();
 
         // ask DisplayModule => reset input fields
-        // display.resertForm();
+        display.resertForm(patientObj);
 
         // ask DisplayModule => update the list of patients
+        display.addPatientToList(patientObj);
     }
 
 
